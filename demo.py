023f61d994def40d88f54066b13d894c92c5716d
ferstar <zhangjianfei3@gmail.com>
2015-03-14 13:16:41 +0800

    修改:         demo.py 重命名:       template/index1.html -> template/index.html

--- demo.py
+++ demo.py
@@ -13,22 +13,14 @@ from application import application
 from tornado.options import define,options
 
 define("port", default=9010, help="run on the given port", type=int)
- 
-def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    print 'Development server is running at http://127.0.0.1:%s/' % options.port
-    print 'Quit the server with Control-C'
-    tornado.ioloop.IOLoop.instance().start()
-     
+   
 if __name__ == '__main__':
-    main()
     tornado.options.parse_command_line()
     def run(mid,port):
         print "Process %d start" % mid
         sys.stdout.flush()
-        app.listen(port)
+        http_server = tornado.httpserver.HTTPServer(application)
+        http_server.listen(port)
         tornado.ioloop.IOLoop.instance().start()
     jobs=list()
     for mid,port in enumerate(range(9010,9014)):
