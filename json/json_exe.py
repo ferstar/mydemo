a60d63798be2cb7d3f93bbb879c0f072e0dcec3e
zhangjianfei <zhangjianfei@marvint.com>
2015-03-23 17:35:29 +0800

    修改：      json_exe.py

--- json/json_exe.py
+++ json/json_exe.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-#from bson import json_util
 import json
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 conn = psycopg2.connect('dbname=surm user=marvin password=marvint')
 cur = conn.cursor(cursor_factory=RealDictCursor)
-cur.execute("SELECT * FROM surm.room")
+cur.execute("SELECT * FROM surm.meeting")
 
-print (json.dumps(cur.fetchall(), ensure_ascii=False))
+print (json.dumps(cur.fetchall(), default=date_handler, ensure_ascii=False))
 
